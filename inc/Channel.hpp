e4fda3d74c7ea984d5a3e91c364334c0c5426195
uninstall-lea <lea.bisson@gmail.com>
2024-01-24 22:18:52 +0100

    update Channel

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -1,66 +1,36 @@
 #pragma once
 
-#include <string>
-#include <iostream>
+#include "IRC.hpp"
 
-/* error codes */
-#define ERR_NEEDMOREPARAMS		461		//"<char> : invalid parameter"
-#define ERR_KEYSET				467		//"<channel> : Channel key already set"
-#define ERR_CHANOPRIVSNEEDED	482		//"<channel> : You're not channel operator"
-#define ERR_USERNOTINCHANNEL	441		//"<nick> <channel> : They aren't on that channel"
-#define ERR_UNKNOWNMODE			472		//"<char> : unknown channel mode"
-#define ERR_NOSUCHCHANNEL		403		//"<channel name> : No such channel"
-#define ERR_INVITEONLYCHAN      473 	//"<channel> : Cannot join channel (+i)"
-#define ERR_BADCHANNELKEY		475		//"<channel> : Cannot join channel (+k)""
-#define ERR_CHANNELISFULL       471     //"<channel> : Cannot join channel (+l)"
+typedef std::vector<Client&> refClients;
+
+class Server;
 
 class	Channel {
 
 	private:
-		std::string	_name;
-		std::string _password;
-		int			_chanCapacity;
-		bool		_usersVisible;
-		bool		_topicEnabled;
-		bool		_inviteOnly;
+		std::string				_name;
+		std::string				_password;
+		int						_chanCapacity;
+		bool					_usersVisible;
+		bool					_topicEnabled;
+		bool					_inviteOnly;
+		refClients				_chanUsers;
+		refClients				_chanOp;
 
 	public:
-					Channel( void );
-					Channel( Channel const& src );
-					~Channel( void );
+								Channel( std::string name, Client &client );
+								~Channel( void );
 
-					Channel& operator=( Channel const& rhs );
+		refClients::iterator	getClientByName( const std::string& name ) const;
 
-	/* channel modes */
-		void		setPassword( std::string password );
-		void		setChanOP( std::string user );
-		void		setChanCapacity( int capacity );
-		void		setTopic( bool status );
-		void		setInviteOnly( bool status );
+		void					setPassword( std::string& password );
+		void					setChanOP( Client& user );
+		void					unsetChanOP( Client& user );
+		void					setChanCapacity( int capacity );
+		void					setTopic( bool status );
+		void					setInviteOnly( bool status );
+
+		void					sendMsg( std::string msg );
 
 };
-
-
-/*----------------------------------------------------------------------------*/
-
-#include "Channel.hpp"
-
-Channel::Channel( void ) {
-
-}
-
-Channel::Channel( Channel const& src ) {
-
-	*this = src;
-}
-
-Channel::~Channel( void ) {
-
-}
-
-Channel&	Channel::operator=( Channel const& rhs ) {
-
-	return (*this);
-}
-
-/*----------------------------------------------------------------------------*/
---
+++ src/Channel.cpp
@@ -0,0 +1,59 @@
+#include "Channel.hpp"
+
+/* -------------------------------------------------------------------------- */
+/*                                 Constructor                                */
+/* -------------------------------------------------------------------------- */
+
+Channel::Channel( std::string name, Client& client ): _name(name), _password(""),
+_chanCapacity(0), _usersVisible(true), _topicEnabled(false), _inviteOnly(false) {
+
+	getServer().addChannel(*this);
+	_chanUsers.push_back(client);
+	_chanOp.push_back(client);
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                   Getter                                   */
+/* -------------------------------------------------------------------------- */
+
+refClients::iterator Channel::getClientByName( const std::string& name ) {
+
+    refClients::iterator it = _chanOp.begin();
+    for (; it != _chanOp.end(); it++)
+        if (name == it->getUserName())
+	        return (it);
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                   Setter                                   */
+/* -------------------------------------------------------------------------- */
+
+void	Channel::setPassword( std::string &password ) {
+
+	_password = password;
+}
+
+void	Channel::setChanOP( Client& user ){
+
+	_chanOp.push_back(user);
+}
+
+void	Channel::unsetChanOP( Client& user ){
+
+	_chanOp.erase(getClientByName(user.getUserName()));
+}
+
+void	Channel::setChanCapacity( int capacity ){
+
+	_chanCapacity = capacity;
+}
+
+void	Channel::setTopic( bool status ){
+
+	_topicEnabled = status;
+}
+
+void	Channel::setInviteOnly( bool status ){
+
+	_inviteOnly == status;
+}
